d38b61804304444f9d914a20d0579d9f6122f982
Sean Voda <36674581+dsvoda@users.noreply.github.com>
2023-05-04 11:03:54 -0500

    Update fetch_non_movable_resources.py
    Extend fetch_non_movable_resources.py to include resource group and region move support

--- fetch_non_movable_resources.py
+++ fetch_non_movable_resources.py
@@ -1,11 +1,11 @@
 # Author: @seanvoda
+# Company: @GobiIt
 # Description: Fetches the list of non-movable resources from the Azure docs and saves them to a JSON file
-# Usage: python fetch_non_movable_resources.py
 # Output: NonMovableResources.json
-# Notes: This script is not meant to be run as part of the build process. It is provided as a convenience for updating the list of non-movable resources.
+# Notes: This script is provided as a convenience for updating the list of non-movable resources.
 # Dependencies: requests, re, json
-# Date: 2023-04-13
-# Version: 1.0.0
+# Date: 2023-05-03
+# Version: 1.1.0
 
 import json
 import re
@@ -19,7 +19,7 @@ non_movable_resource_types = []
 service_pattern = r'^##\s+(.*?)$'
 service_matches = list(re.finditer(service_pattern, response.text, re.IGNORECASE | re.MULTILINE))
 
-row_pattern = r'\|\s*(.*?)(?:\*\*Yes\*\*|\*\*No\*\*)?\s*\|\s*(?:Yes|No)\s*\|\s*No\s*\|'
+row_pattern = r'\|\s*(.*?)\s*\|(.*?)\s*\|(.*?)\s*\|(.*?)\s*\|'
 row_matches = list(re.finditer(row_pattern, response.text, re.IGNORECASE | re.MULTILINE))
 
 service_index = 0
@@ -31,10 +31,19 @@ for row_match in row_matches:
         service_index += 1
 
     full_resource_type = f"{service_matches[service_index].group(1)}/{row_match.group(1)}"
-    
-    # Fix resource type format
+
     full_resource_type = full_resource_type.replace('>', '/').replace('|', '').replace(' ', '').replace('//', '/')
-    non_movable_resource_types.append(full_resource_type.strip())
+    
+    resource_group_move = 'Yes' if '**Yes**' in row_match.group(2) else 'No'
+    subscription_move = 'Yes' if '**Yes**' in row_match.group(3) else 'No'
+    region_move = 'Yes' if 'Yes' in row_match.group(4) else 'No'
+    
+    non_movable_resource_types.append({
+        'ResourceType': full_resource_type.strip(),
+        'ResourceGroup': resource_group_move,
+        'Subscription': subscription_move,
+        'Region': region_move
+    })
 
 with open('NonMovableResources.json', 'w') as outfile:
     json.dump({'ResourceTypes': non_movable_resource_types}, outfile)
